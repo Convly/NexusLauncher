a37c94a7821ffb5e1b720fe40727a47d2702d334
JSX <jean-sebastien.herbaux@epitech.eu>
2017-10-22 01:46:11 +0200

    Remove filesystem include / call (problem with cross plateform behaviour

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <filesystem>
 #include "Launcher.hpp"
-
-namespace fs = std::experimental::filesystem;
 
 int main()
 {
-	std::string path = "./";
-	for (auto & p : fs::directory_iterator(path)) {
-		std::cout << "(" << fs::is_directory(p) << ")" << p << std::endl;
-	}
 
 	nx::Launcher launcher;
 
