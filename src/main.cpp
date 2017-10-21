0f03a33c0b31c296ec624de3d74d9bff500fe255
JSX <jean-sebastien.herbaux@epitech.eu>
2017-10-21 16:06:54 +0100

    basisArchitecture(feat):Adding test for listing directory with filesystem library

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::experimental::filesystem;
 
 int main()
 {
-  std::cout << "Hello World!" << std::endl;
-  return (0);
+	std::string path = "./";
+	for (auto & p : fs::directory_iterator(path)) {
+		std::cout << "(" << fs::is_directory(p) << ")" << p << std::endl;
+	}
+	std::cout << "Hello World!" << std::endl;
+	return (0);
 }
